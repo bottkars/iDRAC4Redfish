047f420dcb40f8fd5ae3fa2a054c33cd390b0e3a
Karsten Bott <Karsten.Bott@bottnet.de>
2017-04-16 07:38:40 +0200

    added acounts

--- iDRAC4Redfish.psm1
+++ iDRAC4Redfish.psm1
@@ -499,3 +499,12 @@ else
 Get-iDRACodata -odata $result.Headers.location
 
 }
+
+
+function Get-iDRACAccounts
+{
+$Myself = $MyInvocation.MyCommand.Name.Substring(9) 
+$Accounts = (Get-iDRACodata $Global:iDRAC_Manager).Members | Get-iDRACodata
+$Accounts.PSTypeNames.Insert(0, $Myself)
+Write-Output $Accounts
+}
