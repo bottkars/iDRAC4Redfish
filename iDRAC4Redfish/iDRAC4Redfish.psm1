48122aecd4c768876f3d6be776c79d40c0a2d91e
Karsten Bott <Karsten.Bott@bottnet.de>
2017-04-18 13:57:25 +0200

    _

--- iDRAC4Redfish/iDRAC4Redfish.psm1
+++ iDRAC4Redfish/iDRAC4Redfish.psm1
@@ -349,7 +349,7 @@ function Get-iDRACManagerElement
     (
         [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,ParameterSetName='1')]
         [Alias("Function")]
-        [ValidateSet('LogServices','NetworkProtocol','')]
+        [ValidateSet('LogServices','NetworkProtocol',' ')]
         $iDRAC_Element
     )
 
