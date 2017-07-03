f58791227f82cd12d7a183a54817f6fc89c30960
bottkars <Karsten.Bott@bottnet.de>
2017-07-03 12:24:23 +0200

    test

--- iDRAC4Redfish.tests.ps1
+++ iDRAC4Redfish.tests.ps1
@@ -3,7 +3,6 @@
 # You need a unit test framework such as Pester to run PowerShell Unit tests. 
 # You can download Pester from http://go.microsoft.com/fwlink/?LinkID=534084
 #
-
 Describe "Get-Function" {
 	Context "Function Exists" {
 		It "Should Return" {
